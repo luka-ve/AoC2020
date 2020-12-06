2ba7a8330e8e64249ffc93542598238d4e81f453
Luka Velinov <luka.velinov@gmail.com>
2020-12-06 21:45:40 +0000

    Added part 2 solution

--- 06.py
+++ 06.py
@@ -8,5 +8,31 @@ def part1(data):
 
     return sum(n_different_questions_per_group)
 
+# Concatenate every group's individual strings and convert to set
+# Compare the element count of every group set to the group's size
 def part2(data):
-    pass
+    groups = data.split('\n\n')
+
+    groups = [group.split('\n') for group in groups]
+
+    # Group sizes are needed to compare the count of each set element per group to the group size
+    group_sizes = [len(group) for group in groups]
+
+    groups_concatenated = [''.join(group) for group in groups]
+
+    # Convert every group string to a tuple, eliminating duplicates
+    group_answered_questions = [tuple(set(q)) for q in groups_concatenated]
+
+    total_count = []
+
+    for i, group in enumerate(group_answered_questions):
+        group_count = 0
+
+        for question in group:
+            if groups_concatenated[i].count(question) == group_sizes[i]:
+                group_count += 1
+        
+        total_count.append(group_count)
+
+
+    return sum(total_count)
